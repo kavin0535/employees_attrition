b70ecf2ba5487fdc7a39296e4bf837d3268ad623
Kavin Prasad S <kavinprasads2006@gmail.com>
2026-03-25 20:04:48 +0530

    fixed port binding

--- src/app.py
+++ src/app.py
@@ -51,5 +51,6 @@ def predict():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    import os
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
